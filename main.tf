206618108dc0307cbea7f77c366e760b36606e27
Crowdbotics <team@crowdbotics.com>
2019-06-21 22:43:18 +0000

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "heroku" {
+  version = "~> 1.5"
+}
+provider "cloudflare" {
+  version = "~> 1.9"
+}
+terraform {
+  backend "s3" {
+    bucket  = "crowdbotics-terraform"
+    key     = "hiago-5002"
+    region  = "us-east-1"
+    encrypt = "true"
+  }
+}
